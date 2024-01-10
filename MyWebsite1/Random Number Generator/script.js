00e6d6df5f5f9472597805d2878b1693591e88f0
Adam Aly <99606116+AlyAdam@users.noreply.github.com>
2024-01-10 22:41:11 +0200

    Delete MyWebsite1 directory

--- MyWebsite1/Random Number Generator/script.js
+++
@@ -1,8 +0,0 @@
-function random_numberl() {
-    var minNum = parseInt(document.querySelector("#value1").value);
-    var maxNum = parseInt(document.querySelector("#value2").value);
-    
-    var calculate = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-
-    document.querySelector("#Result").innerHTML =calculate;
-}
